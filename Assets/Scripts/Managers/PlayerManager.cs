7eb2f82b802e23be8cdbfd1ab9457364b28313fd
Hefe73 <franceshe@esat-alumni.com>
2026-01-31 05:03:32 +0100

    [PROG]: Movement, start minigame 3

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-
-    [SerializeField] GameObject Player;
+    [SerializeField] Transform cam; // Asigna la cámara en el inspector (o se buscará Camera.main / "FollowCamera")
     public float speed = 5f;
-    public float h;
-    public float v;
+    public float rotateSpeed = 10f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float h;
+    float v;
+
     void Start()
     {
-            //Player = GameObject.FindGameObjectWithTag("Player");
+        if (cam == null)
+        {
+            if (Camera.main != null) cam = Camera.main.transform;
+            else
+            {
+                GameObject found = GameObject.Find("FollowCamera");
+                if (found != null) cam = found.transform;
+            }
+        }
     }
 
     void Update()
@@ -19,10 +27,25 @@ public class PlayerManager : MonoBehaviour
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(h, 0f, v);
-        transform.Translate(movement * speed * Time.deltaTime, Space.Self);
+        Vector3 camForward = cam.forward;
+        camForward.y = 0f;
+        camForward.Normalize();
 
-  
-        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Vector3 camRight = cam.right;
+        camRight.y = 0f;
+        camRight.Normalize();
+
+        Vector3 moveDir = camForward * v + camRight * h;
+
+        if (moveDir.sqrMagnitude > 1f) moveDir.Normalize();
+
+        transform.position += moveDir * speed * Time.deltaTime;
+
+        if (moveDir.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotateSpeed * Time.deltaTime);
+        }
     }
 }
+
