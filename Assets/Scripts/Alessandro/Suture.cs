0679df29d279c0c57b9c292cb148876419ea75b9
Guillermo Boscá <Kenhaytplays@gmail.com>
2026-02-01 04:39:20 +0100

    Suture Sounds added

--- Assets/Scripts/Alessandro/Suture.cs
+++ Assets/Scripts/Alessandro/Suture.cs
@@ -16,6 +16,9 @@ public class Suture : MonoBehaviour
     private float remainingTime;
     private bool gameActive = true;
 
+    public AudioSource sutureSfx;
+    public PlayUISound uiSoundPlayer;
+
     private void Start()
     {
         isSutured = new bool[suturePoints.Length];
@@ -29,9 +32,9 @@ public class Suture : MonoBehaviour
     private void Update()
     {
         if (!gameActive) return;
-        
+
         UpdateTimer();
-        
+
         if (Input.GetMouseButtonDown(0))
         {
             CheckSuturePointClick();
@@ -41,7 +44,7 @@ public class Suture : MonoBehaviour
     private void UpdateTimer()
     {
         remainingTime -= Time.deltaTime;
-        
+
         timerText.text = "Tiempo Restante: " + Mathf.Ceil(remainingTime).ToString() + "s";
 
         if (remainingTime <= 0 && gameActive)
@@ -63,6 +66,10 @@ public class Suture : MonoBehaviour
 
             if (clickedPoint == suturePoints[currentPointIndex])
             {
+                sutureSfx.volume = Random.Range(0.85f, 1f);
+                sutureSfx.pitch = Random.Range(1f - 0.15f, 1f + 0.15f);
+                sutureSfx.PlayOneShot(sutureSfx.clip);
+                
                 isSutured[currentPointIndex] = true;
                 lineRenderer.positionCount++;
                 lineRenderer.SetPosition(currentPointIndex, clickedPoint.position);
@@ -87,13 +94,13 @@ public class Suture : MonoBehaviour
         {
             Destroy(activeMarker);
         }
-        
+
         if (currentPointIndex < 1)
         {
             activeMarker = Instantiate(markerPrefab);
-            
+
             activeMarker.transform.SetParent(suturePoints[0]);
-            
+
             activeMarker.transform.localPosition = new Vector3(-0.54f, 0.6f, 0f);
             activeMarker.transform.localRotation = Quaternion.Euler(0f, 0f, -45f);
             activeMarker.transform.localScale = new Vector3(0.2f, 0.2f, 1f);
@@ -104,12 +111,14 @@ public class Suture : MonoBehaviour
     {
         Debug.Log("¡Minijuego completado con éxito!");
         gameActive = false;
-        timerText.text = "¡Sutura Completa!"; 
+        timerText.text = "¡Sutura Completa!";
+        uiSoundPlayer.PlaySoundWin();
     }
 
     private void OnTimeUp()
     {
         gameActive = false;
         timerText.text = "¡Se acabó el tiempo!";
+        uiSoundPlayer.PlaySoundLoose();
     }
 }
